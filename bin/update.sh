baf1db7e4bb5c366d3a53fb6157105335bd7caa6
Alan Jaouen <14994179+alanjaouen@users.noreply.github.com>
2024-09-17 20:55:31 +0000

    wip

--- bin/update.sh
+++ bin/update.sh
@@ -1,7 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rm -rf ${DIR}/pmd/*
-VERSION=$(curl -s https://api.github.com/repos/pmd/pmd/releases/latest | jq --raw-output ".tag_name")
-DOWNLOAD_URL=$(curl -s https://api.github.com/repos/pmd/pmd/releases/latest | jq --raw-output ".assets[].browser_download_url" | grep pmd-bin)
+VERSION=$(gh api repos/pmd/pmd/releases/latest | jq --raw-output ".tag_name")
+DOWNLOAD_URL=$(gh api repos/pmd/pmd/releases/latest | jq --raw-output ".assets[].browser_download_url" | grep bin.zip)
 echo "version=${VERSION}"
 echo "url=${DOWNLOAD_URL}"
 curl $DOWNLOAD_URL -L -o ${DIR}/pmd.zip
